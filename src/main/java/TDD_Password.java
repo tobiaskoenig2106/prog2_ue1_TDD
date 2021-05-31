7c3564828d389196acc692dfabf8b0c54c9e71e9
Tobias_Koenig <tobias.koenig@stud.fh-campuswien.ac.at>
2021-05-31 15:42:58 +0200

    solved for first Test + wrote second test

--- src/main/java/TDD_Password.java
+++ src/main/java/TDD_Password.java
@@ -2,7 +2,9 @@ public class TDD_Password {
 
 
     public static boolean checkPassword(String password) {
-
+        if (password.length() > 25){
+            return false;
+        }
         return true;
     }
 
--- src/test/java/TDD_PasswordTest.java
+++ src/test/java/TDD_PasswordTest.java
@@ -7,10 +7,21 @@ import static org.junit.jupiter.api.Assertions.*;
 class TDD_PasswordTest {
 
 
-
+    /**
+     * Testet ob das Passwort zu lang ist
+     */
     @Test
     void checkPassword_tooLong() {
         String password = "AbCdEfGhIjKlMnOpQrStUvWxYz";
+        assertEquals(false, TDD_Password.checkPassword(password));
+    }
+
+    /**
+     * Testet ob das Passwort zu kurz ist
+     */
+    @Test
+    void checkPassword_tooShort() {
+        String password = "AbCd";
         assertEquals(false, TDD_Password.checkPassword(password));
     }
 
