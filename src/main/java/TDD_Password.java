f20a873216e9457e42e44c0dd3c6fb7b4a00e738
Tobias_Koenig <tobias.koenig@stud.fh-campuswien.ac.at>
2021-05-31 17:18:28 +0200

    solved for ninth Test (followingDigits)

--- src/main/java/TDD_Password.java
+++ src/main/java/TDD_Password.java
@@ -11,27 +11,30 @@ public class TDD_Password {
         boolean hasSpecChar = false;
         int digitcounter = 0;
 
-        if (password.length() > 25 || password.length() < 5){
+        if (password.length() > 25 || password.length() < 5) {
             return false;
         }
-        for (int i = 0;i<password.length();i++){
+        for (int i = 0; i < password.length(); i++) {
             char character = password.charAt(i);
-            if(Character.isLowerCase(character)){
+            if (Character.isLowerCase(character)) {
                 hasLowercaseLetter = true;
             }
-            if(Character.isUpperCase(character)){
+            if (Character.isUpperCase(character)) {
                 hasUppercaseLetter = true;
             }
-            if(Character.isDigit(character)){
+            if (Character.isDigit(character)) {
                 hasDigit = true;
-                if(i+3<password.length() && character == password.charAt(i+1) && password.charAt(i+1) == password.charAt(i+2) && password.charAt(i+2) == password.charAt(i+3)){
+                if (i + 3 < password.length() && character == password.charAt(i + 1) && password.charAt(i + 1) == password.charAt(i + 2) && password.charAt(i + 2) == password.charAt(i + 3)) {
+                    return false;
+                }
+                if (i + 2 < password.length() && character+1 == password.charAt(i + 1) && password.charAt(i + 1)+1 == password.charAt(i + 2)) {
                     return false;
                 }
             }
             Pattern pattern = Pattern.compile("[()#$?!%/@]");
             Matcher specChar = pattern.matcher(Character.toString(character));
             Boolean specialChar = specChar.find();
-            if (specialChar){
+            if (specialChar) {
                 hasSpecChar = true;
             }
             if (!Character.isLowerCase(character) && !Character.isUpperCase(character) && !Character.isDigit(character) &&
